88cdd267a7756c45578a01b45d1222c98d034e25
lprendas <lprendas@estudiantec.cr>
2026-04-28 05:23:15 -0600

    Se implemento desempate de carreras y se mejoro la documentacion del codigo logic

--- logic.pl
+++ logic.pl
@@ -1,45 +1,45 @@
 :- consult('bd.pl').
 
-% ============================
-% Hechos dinámicos (respuestas del usuario)
-% ============================
-
+% Hechos dinamicos para almacenar las preferencias del usuario
 :- dynamic gusta/1.
 :- dynamic no_gusta/1.
 
-
-% ============================
-% LIMPIAR RESPUESTAS
-% ============================
-
+% Limpiar respouestas
 limpiar :-
     retractall(gusta(_)),
     retractall(no_gusta(_)).
 
 
-% ============================
-% PUNTAJE DE UNA PROFESIÓN
-% ============================
+% Puntaje de una profesión basado en las afinidades y antagonias del usuario
+
 
 puntaje_profesion(Profesion, Puntaje) :-
-    findall(1, (gusta(X), afinidad(Profesion, X)), Afinidades),
-    length(Afinidades, Positivos),
-
-    findall(1, (no_gusta(X), afinidad(Profesion, X)), Negativos1),
-    findall(1, (gusta(X), antagonia(Profesion, X)), Negativos2),
-
-    length(Negativos1, N1),
-    length(Negativos2, N2),
-
-    Negativos is N1 + N2,
-
-    Puntaje is Positivos - Negativos.
+    findall(Puntos, puntos_respuesta(Profesion, Puntos), ListaPuntos),
+    sum_list(ListaPuntos, Puntaje).
 
 
-% ============================
-% EVALUAR TODAS LAS PROFESIONES
-% ============================
+% Una afinidad que al usuario le gusta suma 2
+puntos_respuesta(Profesion, 2) :-
+    gusta(X),
+    afinidad(Profesion, X).
 
+% Una afinidad que al usuario NO le gusta resta
+puntos_respuesta(Profesion, -2) :-
+    no_gusta(X),
+    afinidad(Profesion, X).
+
+% Una antagonia que al usuario sí le gusta resta 3
+puntos_respuesta(Profesion, -3) :-
+    gusta(X),
+    antagonia(Profesion, X).
+
+% Una antagonia que al usuario NO le gusta suma 1
+puntos_respuesta(Profesion, 1) :-
+    no_gusta(X),
+    antagonia(Profesion, X).
+
+
+% Evaluar todas las profesiones 
 evaluar_profesiones(Lista) :-
     findall(
         Puntaje-Profesion,
@@ -51,10 +51,7 @@ evaluar_profesiones(Lista) :-
     ).
 
 
-% ============================
-% MEJOR PROFESIÓN
-% ============================
-
+% Mejor profecion basada en el puntaje
 mejor([P-C], C, P).
 
 mejor([P1-C1, P2-C2 | Resto], MejorC, MejorP) :-
@@ -65,18 +62,53 @@ mejor([P1-C1, P2-C2 | Resto], MejorC, MejorP) :-
     ).
 
 
-% ============================
-% RECOMENDAR
-% ============================
-
+% Recomendar la mejor carrera basada en el puntaje
 recomendar(Carrera) :-
     evaluar_profesiones(Lista),
-    mejor(Lista, Carrera, _).
+    mejor(Lista, _, MejorPuntaje),
+    profesiones_empatadas(MejorPuntaje, Empatadas),
+    desempatar(Empatadas, Carrera).
+
+profesiones_empatadas(Puntaje, Empatadas) :-
+    evaluar_profesiones(Lista),
+    findall(
+        Carrera,
+        member(Puntaje-Carrera, Lista),
+        Empatadas
+    ).
 
 
-% ============================
-% MOSTRAR RESULTADO
-% ============================
+
+% Desempate entre carreras 
+desempatar([Carrera], Carrera).
+
+desempatar(Empatadas, MejorCarrera) :-
+    findall(
+        Coincidencias-Carrera,
+        (
+            member(Carrera, Empatadas),
+            contar_afinidades_positivas(Carrera, Coincidencias)
+        ),
+        Lista
+    ),
+    sort(Lista, Ordenada),
+    reverse(Ordenada, [_-MejorCarrera | _]).
+
+
+contar_afinidades_positivas(Carrera, Conteo) :-
+    findall(
+        1,
+        (
+            gusta(X),
+            afinidad(Carrera, X)
+        ),
+        Lista
+    ),
+    length(Lista, Conteo).
+
+
+
+% Mostrar recomendación al usuario
 
 mostrar_recomendacion :-
     recomendar(Carrera),
@@ -84,10 +116,8 @@ mostrar_recomendacion :-
     write(Carrera), nl.
 
 
-% ============================
-% DEBUG (para defensa)
-% ============================
 
+% Debug: Mostrar puntajes de todas las profesiones
 mostrar_puntajes :-
     evaluar_profesiones(L),
     write(L), nl.
